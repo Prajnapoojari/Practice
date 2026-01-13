3aa7f5d5d738d9b380fdcbaaf9d33940979c9177
Prajnapoojari <prajnapoojari098@gmail.com>
2026-01-13 19:39:38 +0530

    initial commit

---
+++ src/WordCount.java
@@ -0,0 +1,8 @@
+public class WordCount {
+    public static void main(String[] args) {
+        String str = "Java is easy to learn";
+
+        String[] words = str.split(" ");
+        System.out.println("Number of words: " + words.length);
+    }
+}
